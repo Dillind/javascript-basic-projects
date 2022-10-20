13bd1e963828e4d5a8e8e1228e81fa936c5e6b1f
Dylan Lindsay <dylan234@tpg.com.au>
2022-10-20 16:40:44 +1000

    completed.

---
+++ 05-sidebar/setup/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
